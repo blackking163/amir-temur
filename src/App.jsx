05c9c73bb7bf0c1e0fe71b55fc5e1eaf5ee76343
blackking163 <maikaljordan2323@gmail.com>
2024-12-26 19:19:05 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.scss"
+import Colom from './Colom/Colom'
+
+const App = () => {
+  return (
+    <>
+      <Colom/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Colom/Colom.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./Colom.scss"
+
+const Colom = () => {
+  return (
+    <>
+     <section className="section">
+
+<div className="title">
+    <h1>СТАНЦИЯ СКВЕР АМИРА ТЕМУРА</h1>
+</div>
+<div className="container">
+<div className="wrapper">
+
+
+
+    <div className="photo">
+        <img  src="/orig.jpg" alt="" />
+    </div>
+    <div className="text">
+        <p>«Сквер Амира Тимура» (узб. Amir Temur xiyoboni) — станция Ташкентского метрополитена.
+
+Пущена в эксплуатацию 6 ноября 1977 года в составе первого участка Чиланзарской линии: «Сабир Рахимов» — «Октябрьской Революции».
+
+Расположена между станциями: «Площадь Независимости» и «Хамида Алимджана».</p>
+    </div>
+
+
+
+</div>
+</div>
+
+</section>
+    </>
+  )
+}
+
+export default Colom
